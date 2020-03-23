141ae4b68479e86918a074efd26e56ae5b60ec03
sjmars <sjmarsnc@gmail.com>
2020-03-23 13:19:26 -0400

    add sorting by fullname

--- src/components/EmployeeRow.js
+++ src/components/EmployeeRow.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const EmployeeRow = ( props ) => {
-    console.log("EmployeeRow: ", props);  
-     
-    return (
-        <tr>
-            <td><img src={props.thumbnail} alt = "picture" /></td>
-            <td>{props.first} {props.last}</td>
-            <td>{props.phone}</td>
-            <td>{props.email}</td>
-            <td>{props.dob}</td>
-        </tr>
-    )
+const EmployeeRow = props => {
+  //   console.log("EmployeeRow: ", props);
+
+  return (
+    <tr>
+      <td>
+        <img src={props.thumbnail} alt="picture" />
+      </td>
+      <td>{props.name}</td>
+      <td>{props.phone}</td>
+      <td>{props.email}</td>
+      <td>{props.dob}</td>
+    </tr>
+  );
 };
 
 export default EmployeeRow;
--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -9,17 +9,19 @@ class EmployeeTable extends Component {
     empList: [],
     displayList: [],
     sortKey: "",
-    search: ""
+    search: "",
+    sortIcon: "fa fa-sort"
   };
 
   getEmployees() {
     axios.get("https://randomuser.me/api/?results=20&nat=us").then(res => {
-      console.log("Results in getEmployees: ", res.data.results);
+      res.data.results.forEach(emp => {
+        emp.fullName = emp.name.first + " " + emp.name.last;
+      });
       this.setState({
         empList: res.data.results,
         displayList: res.data.results
       });
-      console.log("in getEmployees STATE: ", this.state);
     });
   }
 
@@ -28,19 +30,41 @@ class EmployeeTable extends Component {
   }
 
   handleSearchChange = event => {
-    console.log("In handleSearchChange: ", event.target.value);
     this.setState({ search: event.target.value });
     let search = event.target.value.toLowerCase();
-    console.log("search string: ", this.state.search, search);
     const newList = this.state.empList.filter(
-      emp => emp.name.first.substring(0, search.length).toLowerCase() === search
+      emp => emp.fullName.substring(0, search.length).toLowerCase() === search
     );
-    console.log("Subset of empList: ", this.state.newList);
     this.setState({ displayList: newList });
+  };
+
+  handleSortChange = event => {
+    switch (this.state.sortIcon) {
+      case "fa fa-sort":
+      case "fa fa-sort-down":
+        this.setState({ sortIcon: "fa fa-sort-up" });
+        this.sortEmpList("asc");
+        break;
+      case "fa fa-sort-up":
+        this.setState({ sortIcon: "fa fa-sort-down" });
+        this.sortEmpList("desc");
+        break;
+    }
   };
 
   sortEmpList = direction => {
     // sort the employee list by name either up or down
+    let sortedList = [];
+    if (direction === "asc") {
+      sortedList = this.state.empList.sort((a, b) =>
+        a.fullName.localeCompare(b.fullName)
+      );
+    } else {
+      sortedList = this.state.empList.sort((a, b) =>
+        b.fullName.localeCompare(a.fullName)
+      );
+    }
+    this.setState({ displayList: sortedList });
   };
 
   render() {
@@ -53,10 +77,16 @@ class EmployeeTable extends Component {
         <div className="row">
           <div className="col-sm-12">
             <table className="table table-striped mt-5">
-              <thead className="thead-light">
+              <thead>
                 <tr>
                   <th>Picture</th>
-                  <th>Name</th>
+                  <th>
+                    Name &nbsp;&nbsp; &nbsp;{" "}
+                    <i
+                      className={this.state.sortIcon}
+                      onClick={this.handleSortChange}
+                    />
+                  </th>
                   <th>Phone</th>
                   <th>Email Address</th>
                   <th>DOB</th>
@@ -66,8 +96,7 @@ class EmployeeTable extends Component {
                 {this.state.displayList.map(employee => (
                   <EmployeeRow
                     thumbnail={employee.picture.thumbnail}
-                    first={employee.name.first}
-                    last={employee.name.last}
+                    name={employee.fullName}
                     phone={employee.phone}
                     email={employee.email}
                     dob={employee.dob.date.substr(0, 10)}
